f0d5295a943485a88d6ca7d30b9863dcf2c14e11
awesomelewis2007 <60709927+awesomelewis2007@users.noreply.github.com>
2022-03-27 19:37:44 +0100

    Add files via upload

---
+++ src/__main__.py
@@ -0,0 +1,98 @@
+import hashlib
+import sys
+import os
+import hashlib
+if __name__ == "__main__":
+    verbose = False
+    quiet = False
+    version = "0.0.1"
+    for arg in sys.argv:
+        if arg == "--help":
+            print("Usage: hash-util -ls -f <hash> -v <file> <hash> --help --verbose --quiet")
+            exit()
+        if arg == "--version":
+            print("hash-util version: " + version)
+            exit()
+        if arg == "--verbose":
+            verbose = True
+            continue
+        if arg == "--quiet":
+            quiet = True
+            continue
+    for arg in sys.argv:
+        if arg == "-ls":
+            for file in os.listdir():
+                if os.path.isfile(file):
+                    print("File: "+file)
+                    print("MD5:    "+hashlib.md5(open(file, 'rb').read()).hexdigest())
+                    print("SHA224: "+hashlib.sha224(open(file, 'rb').read()).hexdigest())
+                    print("SHA256: "+hashlib.sha256(open(file, 'rb').read()).hexdigest())
+                    print("SHA384: "+hashlib.sha384(open(file, 'rb').read()).hexdigest())
+                    print("\n")   
+            exit()
+        if arg == "-f":
+            method = "MD5"
+            for arg in sys.argv:
+                if "--hash=" in arg:
+                    if arg.replace("--hash=","").upper() == "MD5":
+                        method = "MD5"
+                    if arg.replace("--hash=","").upper() == "SHA224":
+                        method = "SHA224"
+                    if arg.replace("--hash=","").upper() == "SHA256":
+                        method = "SHA256"
+                    if arg.replace("--hash=","").upper() == "SHA384":
+                        method = "SHA384"
+                    if arg.replace("--hash=","").upper() == "SHA512":
+                        method = "SHA384"
+            hash = sys.argv[len(sys.argv) - 1]
+            foundfiles = []
+            for root, dirs, files in os.walk(os.getcwd()):
+                for file in files:
+                    if method == "MD5":
+                        if hash == str(hashlib.md5(open(root+"/"+file, 'rb').read()).hexdigest()):
+                            foundfiles.append(root+"/"+file)
+                    if method == "SHA224":
+                        if hash == str(hashlib.sha224(open(root+"/"+file, 'rb').read()).hexdigest()):
+                            foundfiles.append(root+"/"+file)
+                    if method == "SHA256":
+                        if hash == str(hashlib.sha256(open(root+"/"+file, 'rb').read()).hexdigest()):
+                            foundfiles.append(root+"/"+file)
+                    if method == "SHA384":
+                        if hash == str(hashlib.sha384(open(root+"/"+file, 'rb').read()).hexdigest()):
+                            foundfiles.append(root+"/"+file)
+            print(foundfiles)
+            exit()
+
+        if arg == "-v":
+            method = "MD5"
+            for arg in sys.argv:
+                if "--hash=" in arg:
+                    if arg.replace("--hash=","").upper() == "MD5":
+                        method = "MD5"
+                    if arg.replace("--hash=","").upper() == "SHA224":
+                        method = "SHA224"
+                    if arg.replace("--hash=","").upper() == "SHA256":
+                        method = "SHA256"
+                    if arg.replace("--hash=","").upper() == "SHA384":
+                        method = "SHA384"
+                    if arg.replace("--hash=","").upper() == "SHA512":
+                        method = "SHA384"
+
+            hash = sys.argv[len(sys.argv) - 1]
+            foundfiles = []
+            for root, dirs, files in os.walk(os.getcwd()):
+                for file in files:
+                    if method == "MD5":
+                        if hash == str(hashlib.md5(open(root+"/"+file, 'rb').read()).hexdigest()):
+                            foundfiles.append(root+"/"+file)
+                    if method == "SHA224":
+                        if hash == str(hashlib.sha224(open(root+"/"+file, 'rb').read()).hexdigest()):
+                            foundfiles.append(root+"/"+file)
+                    if method == "SHA256":
+                        if hash == str(hashlib.sha256(open(root+"/"+file, 'rb').read()).hexdigest()):
+                            foundfiles.append(root+"/"+file)
+                    if method == "SHA384":
+                        if hash == str(hashlib.sha384(open(root+"/"+file, 'rb').read()).hexdigest()):
+                            foundfiles.append(root+"/"+file)
+            print(foundfiles)
+            exit()
